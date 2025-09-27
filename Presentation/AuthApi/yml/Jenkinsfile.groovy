fa5d62666a0c6219e98b768dc6fe5aef8f7ab667
Mirze1993 <58223139+Mirze1993@users.noreply.github.com>
2025-09-28 02:40:02 +0400

    3

---
+++ Presentation/AuthApi/yml/Jenkinsfile.groovy
@@ -0,0 +1,50 @@
+﻿pipeline {
+    environment {
+        dockerimagename = "mirze1993/auth-api:B${BUILD_NUMBER}"
+        dockerImage = ""
+    }
+    agent any
+
+    stages {
+        stage('Hello') {
+            steps {
+                echo 'Start'
+            }
+        }
+        stage('Checkout Source') {
+            steps {
+                git credentialsId: 'github',branch: 'main', url: 'https://github.com/Mirze1993/BackendTemp.git'
+            }
+        }
+        
+        
+        stage('Build image') {
+            steps{
+                script {
+                    dockerImage = docker.build("${dockerimagename}", "-f Presentation/AuthApi/Dockerfile .")
+                }
+          }
+        }
+        stage('docker compose up'){
+               steps{
+                   sh "docker compose -f Presentation/AuthApi/yml/docker-compose.yml up -d"      
+               }
+       }
+        
+        
+        // stage('Generate Compose Files') {
+        //     steps {
+        //         script {
+        //             sh """
+        //                 pwd 
+        //                 ls
+        //                 cd Presentation/AuthApi/yml
+        //                 chmod +x gen-compose.sh
+        //                 ./gen-compose.sh 2 ${dockerimagename}
+        //                 docker compose -f docker-compose.generated.yml up -d 
+        //             """
+        //         }
+        //     }
+        // }
+    }
+}
---
+++ Scripts/CommonJenkinsfile.groovy
@@ -0,0 +1,33 @@
+﻿pipeline {
+    agent any
+
+    stages {
+        stage('Start') {
+            steps {
+                echo 'Start'
+            }
+        }
+
+        stage('Read Config') {
+            steps {
+                script {
+                    // deploy.json oxu
+                    def deployConfig = readJSON file: 'Scripts/deploy.json'
+
+                    if (deployConfig.authApi == 1) {
+                        echo "Deploy icazəsi var. 'authApi' pipeline işə salınır..."
+                        build job: 'authApi', wait: false, propagate: false
+                    } 
+                    if (deployConfig.fileApi == 1) {
+                        echo "Deploy icazəsi var. 'authApi' pipeline işə salınır..."
+                        build job: 'fileApi', wait: false, propagate: false
+                    }
+                    if (deployConfig.aiApi == 1) {
+                         echo "Deploy icazəsi var. 'authApi' pipeline işə salınır..."
+                         build job: 'aiApi', wait: false, propagate: false
+                    }
+                }
+            }
+        }
+    }
+}
