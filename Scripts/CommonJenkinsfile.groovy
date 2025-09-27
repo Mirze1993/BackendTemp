5ca19b83ff81babb32708e0225d05ae678bb8675
Mirze1993 <58223139+Mirze1993@users.noreply.github.com>
2025-09-28 03:03:33 +0400

    w

--- Scripts/CommonJenkinsfile.groovy
+++ Scripts/CommonJenkinsfile.groovy
@@ -11,20 +11,22 @@
         stage('Read Config') {
             steps {
                 script {
-                    // deploy.json oxu
+                    // deploy.json faylını oxu
                     def deployConfig = readJSON file: 'Scripts/deploy.json'
 
                     if (deployConfig.authApi == 1) {
                         echo "Deploy icazəsi var. 'authApi' pipeline işə salınır..."
                         build job: 'authApi', wait: false, propagate: false
-                    } 
+                    }
+
                     if (deployConfig.fileApi == 1) {
-                        echo "Deploy icazəsi var. 'authApi' pipeline işə salınır..."
+                        echo "Deploy icazəsi var. 'fileApi' pipeline işə salınır..."
                         build job: 'fileApi', wait: false, propagate: false
                     }
+
                     if (deployConfig.aiApi == 1) {
-                         echo "Deploy icazəsi var. 'authApi' pipeline işə salınır..."
-                         build job: 'aiApi', wait: false, propagate: false
+                        echo "Deploy icazəsi var. 'aiApi' pipeline işə salınır..."
+                        build job: 'aiApi', wait: false, propagate: false
                     }
                 }
             }
